2bb1307b595900a07d08c4be5e9057ad9d0104cd
Jonas andersson <Sunhump@drakedog-mac.lan>
2017-11-21 20:21:51 +0100

    clean

--- gulpfile.js
+++ gulpfile.js
@@ -169,7 +169,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('production', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
