60a01059d3e68efedeb6de7fd9d3b3aa7e2e7830
Jonas andersson <Sunhump@drakedog-mac.lan>
2017-11-22 22:40:49 +0100

    browserify test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,3 @@
 console.log('hello');
 
+import foo from './foo';
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
 const del = require('del');
 const wiredep = require('wiredep').stream;
+const browserify = require('browserify');
+const babelify = require('babelify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 const runSequence = require('run-sequence');
 
 const $ = gulpLoadPlugins();
@@ -29,15 +33,33 @@ gulp.task('styles', () => {
     .pipe(reload({stream: true}));
 });
 
+// gulp.task('scripts', () => {
+//   return gulp.src('app/scripts/**/*.js')
+//     .pipe($.plumber())
+//     .pipe($.if(dev, $.sourcemaps.init()))
+//     .pipe($.babel())
+//     .pipe($.if(dev, $.sourcemaps.write('.')))
+//     .pipe(gulp.dest('.tmp/scripts'))
+//     .pipe(reload({stream: true}));
+// });
+
 gulp.task('scripts', () => {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe($.if(dev, $.sourcemaps.init()))
-    .pipe($.babel())
-    .pipe($.if(dev, $.sourcemaps.write('.')))
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
-});
+  
+    const b = browserify({
+      entries: 'app/scripts/app.js',
+      transform: babelify,
+      debug: true
+    });
+  
+    return b.bundle()
+      .pipe(source('app.js'))
+       .pipe($.plumber())
+      .pipe(buffer())
+      .pipe($.sourcemaps.init({loadMaps: true}))
+       .pipe($.sourcemaps.write('.'))
+       .pipe(gulp.dest('.tmp/scripts'))
+       .pipe(reload({stream: true}));
+  });
 
 function lint(files) {
   return gulp.src(files)
